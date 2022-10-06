dddf1650ed8ea62b11e5b1c32f7a0a5165d35740
MD ABIR HOSSAIN <abir573912@gmail.com>
2022-10-06 21:51:13 +0600

    thirddaydone

--- firstflutterproject/lib/main.dart
+++ firstflutterproject/lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:firstflutterproject/home_page.dart';
+import 'package:firstflutterproject/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firstflutterproject/pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,10 +11,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
-
     return MaterialApp(
-      home: HomePage(),
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
